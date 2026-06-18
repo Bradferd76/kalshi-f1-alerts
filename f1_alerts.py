9199f4507f2497d24afb8ca057d5110641a9a116
Bradferd76 <brad.wassink76@gmail.com>
2026-06-18 16:39:37 -0400

    Update f1_alerts.py

--- f1_alerts.py
+++ f1_alerts.py
@@ -1,11 +1,5 @@
+print("SCRIPT STARTED")
+
 import requests
 
-WEBHOOK = "https://discord.com/api/webhooks/1516494102730899597/ced8oEr4YvTqjMt_PfnEh2_S9zCyR7MRRLevIP1tPNov4BqdkBwam2ovwsltHaEyfV_y"
-
-r = requests.post(
-    WEBHOOK,
-    json={"content": "Hello from GitHub Actions"}
-)
-
-print("Status:", r.status_code)
-print("Response:", r.text)
+print("REQUESTS IMPORTED")
